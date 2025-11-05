3ed491e5c811717af94e3264f7689f65b060eb5a
Alebproject32 <isaacale25@gmail.com>
2025-11-04 20:13:09 -0400

    I changed the order of execution in server.js file

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 
+/* ************************** 
+* "View Engine and Templates"
+****************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
+
 /* ***********************
  * Routes
  *************************/
@@ -18,13 +25,6 @@ app.use(static)
 // Index route
 app.get("/", function(req, res){
   res.render("index", {title: "Home"})})
-
-/* ************************** 
-* "View Engine and Templates"
-****************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Local Server Information
